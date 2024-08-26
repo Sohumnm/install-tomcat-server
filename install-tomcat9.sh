0fb3477329d402d184da73a7ba664f624fdce2ce
Sohumnm <158448220+Sohumnm@users.noreply.github.com>
2024-08-27 02:20:22 +0530

    Update install-tomcat9.sh

--- install-tomcat9.sh
+++ install-tomcat9.sh
@@ -6,6 +6,9 @@ wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.93/bin/apache-tomcat-9.0.93.t
 tar -xvzf apache*.tar.*
 #change directory to bin for sh files
 cd /opt/apache-tomcat-9.0.8/bin
+#remove any similar symlinks
+rm -rf /usr/local/bin/tomcatup
+rm -rf /usr/local/bin/tomcatdown
 #create symlinks to /usr/local/bin so we can just start or stop the tomcat9 using "tomcatup" and "tomcatdown" commands
 ln -s ./startup.sh /usr/local/bin/tomcatup
 ln -s ./shutdown.sh /usr/local/bin/tomcatdown
